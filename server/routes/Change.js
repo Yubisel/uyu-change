c9adc54a539ccc67b632ee57990a6931420ad7df
Yubisel <yubiselv@gmail.com>
2020-01-13 14:00:58 -0300

    pruebas para ver enviroment

--- server/routes/Change.js
+++ server/routes/Change.js
@@ -4,7 +4,8 @@ const router = express.Router();
 router.get('/', (req, res) => {
     res.json({
         status: 200,
-        message: 'route working'
+        message: 'route working',
+        env: process.env.ENV
     });
 });
 
