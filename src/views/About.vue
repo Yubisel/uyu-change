613474ebee732ea6e143e52d9bf7311a3fdaf87b
Yubisel <yubiselv@gmail.com>
2020-01-14 16:59:33 -0300

    formateado codigo de pagina about

--- src/views/About.vue
+++ src/views/About.vue
@@ -27,11 +27,6 @@
                 </p>
                 </v-card-text>
         </v-card>
-        <!-- Cotización Hoy en Uruguay
-        El precio de las monedas está expresado en pesos uruguayos
-        https://uy.cotizacion-dolar.com/cotizacion-hoy-uruguay.php
-        Cotización promedio del dólar estadounidense, del euro, del real y de pesos argentinos, expresada en pesos uruguayos.
-        Los datos y valores de cotizaciones aquí publicados son de carácter meramente informativo.-->
     </v-flex>
 </template>
 
