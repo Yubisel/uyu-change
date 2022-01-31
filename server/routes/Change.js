53323e090bf80eaa36bf0ac96541b3974ef50bf0
Yubisel Vega Alvarez <yubiselv@gmail.com>
2022-01-30 22:50:59 -0300

    Update Change.js

--- server/routes/Change.js
+++ server/routes/Change.js
@@ -5,24 +5,27 @@ const cheerio = require('cheerio');
 const request = require('request');
 
 router.get('/', (req, res) => {
-    let changesValues = [];
-    request("https://uy.cotizacion-dolar.com/cotizacion-hoy-uruguay.php", (err, resp, body) => {
-        if (!err && resp.statusCode == 200) {
-            let $ = cheerio.load(body);
-            $('[itemprop=itemListElement]', '.cotizacion-contenido').each(function () {
-                changesValues.push({
-                    initial: $(this).find('.cotizacion-billete').prop('content'),
-                    name: $(this).find('.cotizacion-billete b').text(),
-                    purchase: Number($(this).find('.cc-2b .cotizacion-num').text()),
-                    sale: Number($(this).find('.cc-3b .cotizacion-num').text())
-                });
-            });
-        }
-        res.json({
-            status: resp.statusCode || 404,
-            changesValues: changesValues
+  let changesValues = [];
+  let purchase, sale;
+  request("https://uy.cotizacion-dolar.com/cotizacion-hoy-uruguay.php", (err, resp, body) => {
+    if (!err && resp.statusCode == 200) {
+      let $ = cheerio.load(body);
+      $('[itemprop=itemListElement]', '.cotizacion-contenido').each(function () {
+        purchase = $(this).find('.cc-2b .cotizacion-num').text().replace("$ ", "");
+        sale = $(this).find('.cc-3b .cotizacion-num').text().replace("$ ", "");
+        changesValues.push({
+          initial: $(this).find('.cotizacion-billete').prop('content'),
+          name: $(this).find('.cotizacion-billete b').text(),
+          purchase: Number(purchase),
+          sale: Number(sale)
         });
+      });
+    }
+    res.json({
+      status: resp.statusCode || 404,
+      changesValues: changesValues
     });
+  });
 });
 
 module.exports = router;
